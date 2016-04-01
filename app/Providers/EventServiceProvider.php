61f79fe4c118a3e4e3d8fe906ea09fbb4facafd6
DraperStudio <hello@draperstudio.tech>
2016-04-01 17:29:28 +0200

    first commit

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Lumen\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            'SocialiteProviders\Twitter\TwitterExtendSocialite@handle',
+        ],
+    ];
+}
